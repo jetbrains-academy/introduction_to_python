05646cf373987150233fc940fa2a53c2cc41f2a2
sofiiako <19rubisco93@gmail.com>
2021-06-24 15:20:16 +0300

    Updated task code in If Statement

--- Condition expressions/If statement/if_statement.py
+++ Condition expressions/If statement/if_statement.py
@@ -1,7 +1,7 @@
 name = "John"
 age = 17
 
-if name == "John" or age == 17:  # check that name is "John" or age is 17. If so print next 2 lines.
+if name == "John" and age == 17:  # Check that name is "John" and age is 17. If so print next 2 lines.
     print("name is John")
     print("John is 17 years old")
 
