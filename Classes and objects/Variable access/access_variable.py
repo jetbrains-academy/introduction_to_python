bf8f61d018a8d8e9bf004505c1625097514abf94
sofiiako <19rubisco93@gmail.com>
2021-06-25 18:44:28 +0300

    Updated Access Variable

--- Classes and objects/Variable access/access_variable.py
+++ Classes and objects/Variable access/access_variable.py
@@ -2,18 +2,19 @@ class MyClass:
     variable1 = 1
     variable2 = 2
 
-    def foo(self):     # we'll explain self parameter later
+    def foo(self):     # We'll explain self parameter later
         print("Hello from function foo")
+
 
 my_object = MyClass()
 my_object1 = MyClass()
 
-my_object.variable2 = 3     # assign a new value to variable2 in my_object
+my_object.variable2 = 3     # Assign a new value to variable2 in my_object
 
 print(my_object.variable2)
 print(my_object1.variable2)
 
-my_object.foo()   # call method foo() of object my_object
+my_object.foo()   # Call method foo() of object my_object
 
 print(my_object.variable1)
 
