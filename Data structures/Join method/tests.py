dcc1891ff8e77cff0673959404ed11ed4054aea0
sofiiako <19rubisco93@gmail.com>
2021-06-25 18:12:48 +0300

    Updated test in Join method

--- Data structures/Join method/tests.py
+++ Data structures/Join method/tests.py
@@ -1,17 +1,24 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
+from test_helper import run_common_tests, failed, passed, get_answer_placeholders, get_file_output
 
 
-def test_answer_placeholders():
-    placeholders = get_answer_placeholders()
-    placeholder = placeholders[0]
-    if "separator.join(fruits)" in placeholder:
+def test_output():
+    default_error = "Your output does not match the expected output!"
+    window = get_answer_placeholders()[0]
+    output = get_file_output()
+    if "I like apples and I like bananas and I like peaches and I like grapes" in output:
         passed()
-    else:
-        failed()
+        return
+    if "separator" not in window:
+        failed("Use the predefined separator!")
+        return
+    if ".join(" not in window:
+        failed("Use the join method!")
+        return
+    failed(default_error)
 
 
 if __name__ == '__main__':
     run_common_tests()
-    test_answer_placeholders()
+    test_output()
 
 
