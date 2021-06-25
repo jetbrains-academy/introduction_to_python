a8522d29dace737906420c2580d0ce5f5916c1c7
sofiiako <19rubisco93@gmail.com>
2021-06-25 20:02:09 +0300

    Updated Import Module

--- Modules and packages/Import module/imports.py
+++ Modules and packages/Import module/imports.py
@@ -1,7 +1,7 @@
 import calculator
 import my_module
 
-calc = calculator.Calculator()  # create new instance of Calculator class defined in calculator module
+calc = calculator.Calculator()  # Create new instance of Calculator class defined in calculator module
 calc.add(2)
 print(calc.get_current())
 
