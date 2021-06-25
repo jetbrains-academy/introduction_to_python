f1b793ad47ecbfbd0164787824b818f657da34cb
sofiiako <19rubisco93@gmail.com>
2021-06-25 20:04:53 +0300

    Updated From Import and Packages

--- Modules and packages/From import/from_import.py
+++ Modules and packages/From import/from_import.py
@@ -2,7 +2,7 @@ from calculator import Add
 from my_module import hello as hey
 
 
-calc = Add()  # name `Calculator` used directly without prefix `calculator`
+calc = Add()  # Name `Calculator` used directly without prefix `calculator`
 calc.add(2)
 print(calc.get_current())
 
