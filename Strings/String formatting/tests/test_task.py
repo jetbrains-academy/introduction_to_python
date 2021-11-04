a248b013e0901efbd06f36c74d45c3d1272e40e8
sofiiako <19rubisco93@gmail.com>
2021-11-04 15:20:28 +0300

    Updated test in String Formatting

---
+++ Strings/String formatting/tests/test_task.py
@@ -0,0 +1,25 @@
+import contextlib
+import io
+import unittest
+import re
+
+f = io.StringIO()
+with contextlib.redirect_stdout(f):
+    import string_formatting
+output = f.getvalue().split('\n')
+result_str = output[1]
+correct_string = "I'm \d+ years old"
+
+
+class TestCase(unittest.TestCase):
+    def test_out_len(self):
+        self.assertEqual(len(output), 3, msg='Do not remove or add any print statements.')
+
+    def test_string(self):
+        self.assertIsNotNone(re.match(correct_string, result_str), msg='The result string does not match the expected.')
+
+    def test_string_age(self):
+        self.assertIsNotNone(re.search('\d+', result_str), msg='The result string does not contain your age.')
+
+
+
