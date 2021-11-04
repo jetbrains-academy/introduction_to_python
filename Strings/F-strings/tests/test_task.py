3afa1031f79feae19b6fa50ebe7c2d007c05f007
sofiiako <19rubisco93@gmail.com>
2021-11-04 15:36:44 +0300

    Updated tests in F-Strings

---
+++ Strings/F-strings/tests/test_task.py
@@ -0,0 +1,31 @@
+import contextlib
+import io
+import unittest
+import re
+
+f = io.StringIO()
+with contextlib.redirect_stdout(f):
+    from f_strings import name, age
+output = f.getvalue().split('\n')
+result_str = output[0]
+correct_string = f"Hello, My name is {name} and I'm {age} years old."
+
+
+class TestCase(unittest.TestCase):
+    def test_out_len(self):
+        self.assertEqual(len(output), 2, msg='Do not remove or add any print statements.')
+
+    def test_name(self):
+        self.assertTrue(type(name) == str, msg='Your name should be a string.')
+
+    def test_string_age(self):
+        self.assertIsNotNone(re.search('\d+', result_str), msg='The resulting string should contain '
+                                                               'your age written in digits.')
+    def test_age(self):
+        self.assertIsNotNone(re.search('\d+', str(age)), msg='`age` should contain digits')
+
+    def test_string(self):
+        self.assertIsNotNone(re.match(correct_string, result_str), msg='The result string does not match the expected.')
+
+
+
