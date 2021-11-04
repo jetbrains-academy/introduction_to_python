049a6e98668c35b8263cccbae8fb3b13b6f7c92d
sofiiako <19rubisco93@gmail.com>
2021-11-04 15:02:08 +0300

    Updated test in Basic String Methods

--- Strings/Basic string methods/tests/test_task.py
+++ Strings/Basic string methods/tests/test_task.py
@@ -11,10 +11,8 @@ correct_string = 'MONTY PYTHON'
 
 
 class TestCase(unittest.TestCase):
-    def test_string(self):
-        self.assertEqual(result_str, correct_string, msg='Wrong result string.')
-
     def test_out_len(self):
         self.assertEqual(len(output), 4, msg='Do not remove or add any print statements.')
 
-
+    def test_string(self):
+        self.assertEqual(result_str, correct_string, msg='Wrong result string.')
