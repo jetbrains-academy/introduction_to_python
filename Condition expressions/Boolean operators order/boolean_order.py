d44071d41e0e406199eb0a8fa8e0aba578ebedac
sofiiako <19rubisco93@gmail.com>
2021-11-08 16:58:39 +0300

    Updated tests and task in Boolean Order

--- Condition expressions/Boolean operators order/boolean_order.py
+++ Condition expressions/Boolean operators order/boolean_order.py
@@ -5,4 +5,4 @@ print(name == "John" or not age > 17)
 
 print(name == "John" and not age == 17)
 
-print(name is "Ellis" or not (name is "John" and age == 17))
+print((name == "John" or name == "Jane") and 25 > age >= 16)
--- Condition expressions/Boolean operators order/tests/test_task.py
+++ Condition expressions/Boolean operators order/tests/test_task.py
@@ -4,14 +4,14 @@ import io
 
 f = io.StringIO()
 with contextlib.redirect_stdout(f):
-    import boolean_operators
+    import boolean_order
 output = f.getvalue().split('\n')
-result = output[1]
+result = output[2]
 
 
 class TestCase(unittest.TestCase):
     def test_out_len(self):
-        self.assertEqual(len(output), 3, msg='Please do not remove or add any print statements.')
+        self.assertEqual(len(output), 4, msg='Please do not remove or add any print statements.')
 
     def test_out_str(self):
         self.assertEqual(result, 'True', msg='Your expression does not seem to product the correct result.')
