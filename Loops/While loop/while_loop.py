50fc844ae275e6bc926f7e55b67599293638a7b1
sofiiako <19rubisco93@gmail.com>
2021-11-10 19:52:59 +0300

    Added back the accidentally deleted file in while loop

---
+++ Loops/While loop/while_loop.py
@@ -0,0 +1,16 @@
+square = 1  # Variable introduced to keep track of the iterations
+
+while square <= 10:
+    print(square)    # This code is executed 10 times
+    square += 1      # This code is executed 10 times
+
+print("Finished")  # This code is executed once
+
+square = 0
+number = 1
+
+# Print all squares from 0 to 99.
+while number < 10:
+    square = number ** 2
+    print(square)
+    number += 1
