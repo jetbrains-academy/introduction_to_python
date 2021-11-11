38f49728da78070e78ea3acbf84a7d655c27cfa9
sofiiako <19rubisco93@gmail.com>
2021-11-12 01:02:55 +0300

    Improved test update for string indexing

--- Strings/String indexing/tests/test_task.py
+++ Strings/String indexing/tests/test_task.py
@@ -1,15 +1,13 @@
 import unittest
 
-try:
-    from string_indexing import python, p_letter
 
-    class TestCase(unittest.TestCase):
-        def test_add(self):
+class TestCase(unittest.TestCase):
+    def test_indexing(self):
+        try:
+            from string_indexing import python, p_letter
             self.assertEqual(p_letter, python[0], msg="Make sure you get the first letter from the word Python.")
 
-except ImportError:
-    class TestCase(unittest.TestCase):
-        def test_fail(self):
+        except ImportError:
             self.assertEqual(1, 2, msg="Do not rename any variables.")
 
 
