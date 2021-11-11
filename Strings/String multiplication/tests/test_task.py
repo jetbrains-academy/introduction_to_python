5fb8e28314a551db77ef35970e871fa0dfea8546
sofiiako <19rubisco93@gmail.com>
2021-11-12 01:01:21 +0300

    Improved test update for string multiplication

--- Strings/String multiplication/tests/test_task.py
+++ Strings/String multiplication/tests/test_task.py
@@ -1,16 +1,11 @@
 import unittest
 
-try:
-    from string_multiplication import food
 
-    class TestCase(unittest.TestCase):
-        def test_add(self):
-            self.assertEqual(food, 'couscous', msg="Wrong string result.")
+class TestCase(unittest.TestCase):
+    def test_food(self):
+        try:
+            from string_multiplication import food
+            self.assertEqual('couscous', food, msg="Wrong string result.")
 
-except ImportError:
-    class TestCase(unittest.TestCase):
-        def test_fail(self):
-            self.assertEqual(1, 2, msg="Do not rename any variables.")
-
-
-
+        except ImportError:
+            self.assertTrue(False, msg="Do not rename any variables.")
