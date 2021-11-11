d80933429a77b6680fb934be00a382c3904ea76e
sofiiako <19rubisco93@gmail.com>
2021-11-12 00:57:41 +0300

    Improved test update for concatenation

--- Strings/Concatenation/tests/test_task.py
+++ Strings/Concatenation/tests/test_task.py
@@ -1,19 +1,14 @@
 import unittest
 
-try:
-    from concatenation import hello_world
 
-    class TestCase(unittest.TestCase):
-        def test_add(self):
-            self.assertEqual(hello_world, 'Hello World', msg="Wrong string result.")
-
-        def test_space(self):
+class TestCase(unittest.TestCase):
+    def test_import_hello(self):
+        try:
+            from concatenation import hello_world
+            self.assertEqual('Hello World', hello_world, msg="Wrong string result.")
             self.assertTrue(' ' in hello_world, msg='String result should contain a space.')
 
-except ImportError:
-    class TestCase(unittest.TestCase):
-        def test_fail(self):
-            self.assertEqual(1, 2, msg="Do not rename any variables.")
-
+        except ImportError:
+            self.assertTrue(False, msg="Do not rename any variables.")
 
 
