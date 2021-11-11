dd4c2ef8437c156cc548609b45ac9c09c114fb62
sofiiako <19rubisco93@gmail.com>
2021-11-12 01:10:13 +0300

    Improved test update for in string length

--- Strings/String length/tests/test_task.py
+++ Strings/String length/tests/test_task.py
@@ -5,16 +5,16 @@ try:
 
     class TestCase(unittest.TestCase):
         def test_string(self):
-            self.assertEqual(first_half, phrase[:index_to_slice], msg="You seem to have gotten a wrong slice.")
+            self.assertEqual(phrase[:index_to_slice], first_half, msg="You seem to have gotten a wrong slice.")
 
         def test_index_type(self):
             self.assertTrue(type(index_to_slice) == int, msg="Index should be an integer.")
 
         def test_index(self):
-            self.assertEqual(index_to_slice, int(len(phrase) / 2), msg="You got a wrong slicing index.")
+            self.assertEqual(int(len(phrase) / 2), index_to_slice, msg="You got a wrong slicing index.")
 
 
 except ImportError:
     class TestCase(unittest.TestCase):
         def test_fail(self):
-            self.assertEqual(1, 2, msg="Do not rename any variables.")
+            self.assertTrue(False, msg="Do not rename any variables.")
