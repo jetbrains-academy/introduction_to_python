be884b3d62e1c7fe05ede60dad699477df6a3c9d
sofiiako <19rubisco93@gmail.com>
2021-12-29 17:11:46 +0300

    Added task List Comprehension

--- Data structures/Nested Lists/tests/test_task.py
+++ Data structures/Nested Lists/tests/test_task.py
@@ -10,6 +10,6 @@ output = f.getvalue().split('\n')
 
 
 class TestCase(unittest.TestCase):
-    def test_loop(self):
+    def test_list(self):
         self.assertEqual('9', output[0], msg='The second printed element should be 9.')
         self.assertEqual('10', output[1], msg='The second printed element should be 10.')
---
+++ Loops/List Comprehension/tests/test_task.py
@@ -0,0 +1,11 @@
+import unittest
+
+from task import *
+
+
+# todo: replace this with an actual test
+class TestCase(unittest.TestCase):
+    def test_add(self):
+        test_list  = [i + 10 for i in my_inefficient_list]
+        self.assertEqual(test_list, my_efficient_list,
+                         msg="Each element of `my_efficient_list` should equal a corresponding element of `my_inefficient_list` plus 10.")
