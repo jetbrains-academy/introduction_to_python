bbf47f883068b28ac39ffeeff2f51198810da2de
Igor <igor.gerasimov@jetbrains.com>
2022-01-13 11:42:10 +0300

    feat(Variables - Undefined Variable): Undefined Variable tests updated
    Closes https://youtrack.jetbrains.com/issue/EDC-556

---
+++ Variables/Undefined variable/tests/test_task.py
@@ -0,0 +1,34 @@
+import importlib
+from io import StringIO
+import sys
+import unittest
+from unittest.mock import patch
+
+
+def try_import():
+    import undefined_variable
+    return undefined_variable
+
+
+class TestCase(unittest.TestCase):
+    task_name = 'undefined_variable'
+
+    def setUp(self):
+        try:
+            # Stores output from print() in actualOutput
+            with patch('sys.stdout', new=StringIO()) as self.actualOutput:
+                # Loads submission on first test, reloads on subsequent tests
+                if self.task_name in sys.modules:
+                    importlib.reload(sys.modules[self.task_name])
+                else:
+                    importlib.import_module(self.task_name)
+        except NameError as ne:
+            pass
+        except Exception as e:
+            self.fail("There was a problem while loading the solution - {0}. Check the solution for "
+                      "IDE-highlighted  errors and warnings.".format(str(e)))
+
+    def test_variable_name(self):
+        with self.assertRaises(NameError, msg="Try printing a variable, which was not defined"):
+            try_import()
+
