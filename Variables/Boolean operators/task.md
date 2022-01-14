09d6ad2b98b087572a38f3126df910215925ec00
Igor <igor.gerasimov@jetbrains.com>
2022-01-14 17:09:14 +0300

    feat(Variables - Boolean Operators): tests updated to unittest, task adjusted
    Closes https://youtrack.jetbrains.com/issue/EDC-556

--- Variables/Boolean operators/task.md
+++ Variables/Boolean operators/task.md
@@ -4,7 +4,13 @@ Boolean is a type of value that can only be `True` or `False`. The `==` (equalit
 compares two variables and checks whether they are equal.  
 
   
-Check whether the variable `two` is equal to `three`.
+ - Check whether the variable `two` is equal to `three`.
+ - Check if variable `is_equal` has a deceiving name.
+ - Check if the variable `is_false` does indeed contain a lie.
 
 
 <div class='hint'>Use the <code>==</code> operator.</div>
+
+<div class='hint'>Use the <code>True</code> value.</div>
+
+<div class='hint'>Use the <code>False</code> value.</div>
---
+++ Variables/Boolean operators/tests/test_task.py
@@ -0,0 +1,65 @@
+import importlib
+from io import StringIO
+import sys
+import unittest
+from unittest.mock import patch
+
+
+def try_import():
+    import boolean_operators
+    return boolean_operators
+
+
+class TestCase(unittest.TestCase):
+    task_name = 'boolean_operators'
+
+    def setUp(self):
+        try:
+            # Stores output from print() in actualOutput
+            with patch('sys.stdout', new=StringIO()) as self.actualOutput:
+                # Loads submission on first test, reloads on subsequent tests
+                if self.task_name in sys.modules:
+                    importlib.reload(sys.modules[self.task_name])
+                else:
+                    importlib.import_module(self.task_name)
+        except NameError as ne:
+            pass
+        except Exception as e:
+            self.fail("There was a problem while loading the solution - {0}. Check the solution for "
+                      "IDE-highlighted  errors and warnings.".format(str(e)))
+
+    def test_true(self):
+        expected_value = True
+
+        try:
+            actual_value = try_import().is_true
+        except AttributeError:
+            self.fail(msg="The variable is_true seems to be undefined. "
+                          "Do not remove it from the task code")
+
+        self.assertEqual(expected_value, actual_value, msg="The is_true has a wrong value. "
+                                                                 "Make sure you comparison is to a correct value")
+
+    def test_false(self):
+        expected_value = False
+
+        try:
+            actual_value = try_import().is_false
+        except AttributeError:
+            self.fail(msg="The variable is_false seems to be undefined. "
+                          "Do not remove it from the task code")
+
+        self.assertEqual(expected_value, actual_value, msg="The is_false has a wrong value. "
+                                                           "Make sure you comparison is to a correct value")
+
+    def test_equality_operator(self):
+        expected_value = False
+
+        try:
+            actual_value = try_import().is_equal
+        except AttributeError:
+            self.fail(msg="The variable is_equal seems to be undefined. "
+                          "Do not remove it from the task code")
+
+        self.assertEqual(expected_value, actual_value, msg="The is_equal has a wrong value. "
+                                                           "Make sure you comparison is to a correct value")
