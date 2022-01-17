13dffb4832df8d68bc70f624f5fc7ba7dcfcbd7f
Igor <igor.gerasimov@jetbrains.com>
2022-01-17 14:55:08 +0300

    feat(Functions - Args and kwargs): tests updated to unittest, task adjusted
    Closes https://youtrack.jetbrains.com/issue/EDC-556

--- Functions/Args and kwargs/args_kwargs.py
+++ Functions/Args and kwargs/args_kwargs.py
@@ -21,16 +21,18 @@ random_dialogue("Library", *lst, **dic)  # Call 2 - the exact same output
 
 
 def cat(food, *args, state='still hungry', action='meow', breed='Siamese'):
-    print("-- This cat would", action, end=' ')
-    print("if you gave it", food)
-    print("-- Lovely fur, the", breed)
-    print("-- It's", state, "!")
+    print(f"-- This cat would {action}", end=' ')
+    print(f"if you gave it {food}")
+    print(f"-- Lovely fur, the {breed}")
+    print(f"-- It's {state}!")
     for arg in args:
         print(arg.upper())
+
 
 # Add a list of phrases that will be capitalized.
 phrases = ['It is too fat.', 'You are feeding your cat too much.']
 # Add a dict of keyword arguments.
 keywords = {'state': 'fat', 'action': 'eat', 'breed': 'Maine Coon'}
+keywords.values()
 # Call the cat() function like in example above to print the required output.
 cat('anything', *phrases, **keywords)
