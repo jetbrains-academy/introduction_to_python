5a0469278bcced6eb5e99dccbaaac3df923ca8eb
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-06-01 21:02:48 +0200

    reversed some unintended changes

--- Modules and packages/Executing modules as scripts/task.py
+++ Modules and packages/Executing modules as scripts/task.py
@@ -5,5 +5,5 @@ some_module.func()
 
 # Make a change here.
 if __name__ == "__main__":
-    print('This should be printed ONLY when order.py is called directly.')
+    print('This should be printed ONLY when task.py is called directly.')
 
