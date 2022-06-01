5cddfc094ac4caba95dae79eefb694497714749f
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-06-01 12:24:51 +0200

    Fixed typo in code. Closes https://github.com/jetbrains-academy/introduction_to_python/issues/86

--- Condition expressions/Else and elif parts in if statement/else_elif.py
+++ Condition expressions/Else and elif parts in if statement/else_elif.py
@@ -7,7 +7,7 @@ elif x == 0:
 elif x == 1:
     print('x == 1')                    # If it's not true that x < 0 and x != 0, check if x == 1
 else:
-    print('non of the above is true')
+    print('none of the above is true')
 
 name = "John"
 
