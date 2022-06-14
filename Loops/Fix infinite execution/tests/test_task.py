86705097cdff1da3485dd3c3aec431d19aa1cbaf
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-06-14 14:29:07 +0200

    Replaced timeout_decorator with wrapt_timeout_decorator

--- Loops/Fix infinite execution/tests/test_task.py
+++ Loops/Fix infinite execution/tests/test_task.py
@@ -2,13 +2,13 @@ import unittest
 import contextlib
 import io
 
-import timeout_decorator
+import wrapt_timeout_decorator
 
 from task import should_not_be_infinite
 
 
 class TestCaseWithTimeouts(unittest.TestCase):
-    @timeout_decorator.timeout(2)
+    @wrapt_timeout_decorator.timeout(2)
     def test_that_can_take_too_long(self):
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
