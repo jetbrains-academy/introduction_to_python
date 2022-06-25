56121f1e1d636aa012ab4744eefad43f685b65a5
irish <ladygina.irina.petrovana@gmail.com>
2022-06-25 22:39:59 +0300

    remove unnecessary sentence because it leads to user confusion

--- File input output/Write to file/tests/test_task.py
+++ File input output/Write to file/tests/test_task.py
@@ -1,23 +1,15 @@
 import unittest
-import write_to_file
-
 
 class TestCase(unittest.TestCase):
-    def test_first_line(self):
-        with open('output.txt', 'r') as result:
-            lines = result.readlines()
-            expected, actual = 'This is the output file.\n', lines[0]
-            self.assertEqual(expected, actual,
-                             msg='The first line in the output file should remain "This is the output file.".')
     def test_last_line(self):
-        with open('output.txt', 'r') as result:
+        with open('../output.txt', 'r') as result:
             lines = result.readlines()
             actual = lines[-1]
             self.assertTrue('15' == actual or '15\n' == actual,
                              msg='The last line in the output should be "15".')
 
     def test_second_to_last_line(self):
-        with open('output.txt', 'r') as result:
+        with open('../output.txt', 'r') as result:
             lines = result.readlines()
             expected, actual = 'lion and elephant and monkey\n', lines[-2]
             self.assertEqual(expected, actual,
