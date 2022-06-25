230631eb72b694da3c2151b003bc4eacc7a82cae
irish <ladygina.irina.petrovana@gmail.com>
2022-06-25 22:42:59 +0300

    fix

--- File input output/Write to file/tests/test_task.py
+++ File input output/Write to file/tests/test_task.py
@@ -2,14 +2,14 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_last_line(self):
-        with open('../output.txt', 'r') as result:
+        with open('output.txt', 'r') as result:
             lines = result.readlines()
             actual = lines[-1]
             self.assertTrue('15' == actual or '15\n' == actual,
                              msg='The last line in the output should be "15".')
 
     def test_second_to_last_line(self):
-        with open('../output.txt', 'r') as result:
+        with open('output.txt', 'r') as result:
             lines = result.readlines()
             expected, actual = 'lion and elephant and monkey\n', lines[-2]
             self.assertEqual(expected, actual,
