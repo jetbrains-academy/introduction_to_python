437412b6e65898fd503c99b2e04ddff385737dba
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-06-27 16:04:20 +0200

    test(return value tests): added a test for timeout in case of missing or improper value incrementation resulting in an infinite loop
    based on user feedback
    https://github.com/jetbrains-academy/introduction_to_python/issues/103

--- Functions/Return value/tests/test_task.py
+++ Functions/Return value/tests/test_task.py
@@ -3,11 +3,12 @@ from io import StringIO
 import sys
 import unittest
 from unittest.mock import patch
+import wrapt_timeout_decorator
 
 
 def try_import():
-    import  return_keyword
-    return  return_keyword
+    import return_keyword
+    return return_keyword
 
 
 class TestCase(unittest.TestCase):
@@ -28,16 +29,23 @@ class TestCase(unittest.TestCase):
             self.fail("There was a problem while loading the solution – {0}. Check the solution for "
                       "IDE-highlighted errors and warnings.".format(str(e)))
 
+    @wrapt_timeout_decorator.timeout(2, timeout_exception=TimeoutError)
     def test_keyword_return(self):
-        expected_fib_22_result = [1, 1, 2, 3, 5, 8, 13, 21]
-
         try:
-            actual_fib = try_import().fib
-        except AttributeError:
-            self.fail(msg="There was a problem while loading the fib function. Make sure its declaration stays intact.")
+            expected_fib_22_result = [1, 1, 2, 3, 5, 8, 13, 21]
 
-        actual_fib_22_result = actual_fib(22)
+            try:
+                actual_fib = try_import().fib
+            except AttributeError:
+                self.fail(msg="There was a problem while loading the fib function. Make sure its declaration stays intact.")
 
-        self.assertEqual(expected_fib_22_result, actual_fib_22_result, msg="The Fibonacci sequence up to 22 is "
-                                                                           "incorrect. Make sure you return the result "
-                                                                           "needed.")
+            actual_fib_22_result = actual_fib(22)
+
+            self.assertEqual(expected_fib_22_result, actual_fib_22_result, msg="The Fibonacci sequence up to 22 is "
+                                                                               "incorrect. Make sure you return the result "
+                                                                               "needed.")
+        except OSError as e:
+            self.fail("There was a problem while running the solution - {0}. You might have"
+                      "written an improper value incrementation (likely for b),"
+                      "which resulted in an infinite while loop.".format(str(e)))
+
