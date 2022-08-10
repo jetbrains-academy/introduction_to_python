0fb01d6e99bc424cc76dca1665ef70895070140f
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-08-10 16:17:15 +0200

    feat(task): add dictionary keys task

---
+++ Data structures/Dictionary keys/task.py
@@ -0,0 +1,14 @@
+ages_dict = {
+    "Alice": 21,
+    "Bob": 39,
+    "George": 30,
+    "Susanne": 27,
+    "Bob2": 19,
+    "Ashley": 35,
+    "Alex": 35,
+    "Nancy": 35
+}
+
+
+if __name__ == "__main__":
+    print(ages_dict.values())
