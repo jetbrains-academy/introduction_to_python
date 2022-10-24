53c7b6eb3d5e343e651f83c3e1a4fe6cbc2dad83
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-10-24 15:55:52 +0200

    Added more tests to String Multiplication task

--- Strings/String multiplication/tests/test_task.py
+++ Strings/String multiplication/tests/test_task.py
@@ -9,3 +9,14 @@ class TestCase(unittest.TestCase):
 
         except ImportError:
             self.assertTrue(False, msg="Do not rename any variables.")
+
+    def test_0_code_len(self):
+        with open("string_multiplication.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            self.assertTrue(len(lines) == 3, msg="Please do not add or remove any lines from the code file.")
+
+    def test_statement_1(self):
+        with open("string_multiplication.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            code = lines[-2]
+            self.assertTrue("*" in code, msg="Your solution does not use string multiplication.")
