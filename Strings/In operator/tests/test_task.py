07e7aa6f9197f178dc100b613bb5fe5c1f8ca627
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-10-24 16:20:34 +0200

    Added more tests other tasks in Strings

--- Strings/In operator/tests/test_task.py
+++ Strings/In operator/tests/test_task.py
@@ -9,3 +9,15 @@ class TestCase(unittest.TestCase):
 
         except ImportError:
             self.assertTrue(False, msg="Do not rename any variables.")
+
+    def test_0_code_len(self):
+        with open("in_operator.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            self.assertTrue(len(lines) == 5, msg="Please do not add or remove any lines from the code file.")
+
+    def test_statement_1(self):
+        with open("in_operator.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            code = lines[-2]
+            if not ("in" in code):
+                self.fail(msg="Your solution does not use the in operator.")
--- Strings/String negative indexing/tests/test_task.py
+++ Strings/String negative indexing/tests/test_task.py
@@ -9,3 +9,15 @@ class TestCase(unittest.TestCase):
 
         except ImportError:
             self.assertTrue(False, msg="Do not rename any variables.")
+
+    def test_0_code_len(self):
+        with open("negative_indexing.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            self.assertTrue(len(lines) == 3, msg="Please do not add or remove any lines from the code file.")
+
+    def test_statement_1(self):
+        with open("negative_indexing.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            code = lines[-2]
+            if not ("[-1]" in code):
+                self.fail(msg="Your solution does not use string indexing.")
