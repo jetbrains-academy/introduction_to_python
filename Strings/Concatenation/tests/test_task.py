8b2b3d2a3a09c2af9fd627768bf1ddcd16f0cc33
Sofia Kolchanova <sofiia.kolchanova@gmail.com>
2022-10-24 15:52:51 +0200

    Added more tests to Strings / Concatenation task

--- Strings/Concatenation/tests/test_task.py
+++ Strings/Concatenation/tests/test_task.py
@@ -11,4 +11,13 @@ class TestCase(unittest.TestCase):
         except ImportError:
             self.assertTrue(False, msg="Do not rename any variables.")
 
+    def test_0_code_len(self):
+        with open("concatenation.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            self.assertTrue(len(lines) == 5, msg="Please do not add or remove any lines from the code file.")
 
+    def test_statement_1(self):
+        with open("concatenation.py", "r") as taskfile:
+            lines = taskfile.readlines()
+            code = lines[-2]
+            self.assertTrue("+" in code, msg="Your solution does not use concatenation.")
