ab382f766ab6b373b017f3251b79c57617538fc4
geravant <igor.gerasimov@jetbrains.com>
2023-05-23 18:28:47 +0300

    Demo 1 created

--- Variables/Arithmetic operators/arithmetic_operators.py
+++
@@ -1,9 +0,0 @@
-number = 9.0        # A number.
-
-division_result = number / 2
-
-multiplication_result = number * 2
-
-addition_result = multiplication_result + division_result
-
-print("result = " + str(addition_result))
